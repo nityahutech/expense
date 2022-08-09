3a0d7010dba147c4d8db9dd5c346935d029b391c
Jatin <106946126+JATINHUTECH@users.noreply.github.com>
2022-08-09 09:35:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from "./pages/home/Home";
 
-function App() {
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Quick.
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}/>
+           
+            
+          </Route>
+     
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

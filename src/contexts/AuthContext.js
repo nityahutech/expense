a6f8f51c551dcf630078af6bddcbd7eda35dc2c9
swethahutech <107832376+swethahutech@users.noreply.github.com>
2022-08-18 08:51:07 +0530

    Created AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,14 @@
+import { auth } from "../firebase-config"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+export function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  export function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  export function logout() {
+    return signOut(auth)
+  }

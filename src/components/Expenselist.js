5f2c11b7b86220c6cd8b0eb7dfddd29ce9a3f44f
swethahutech <swetha.vijay@hutechsolutions.com>
2022-08-23 11:37:07 +0530

    corrected modal

--- src/components/Expenselist.js
+++ src/components/Expenselist.js
@@ -539,11 +539,10 @@ function ExpenseList() {
       <Modal
         title="Expense Register"
         visible={isModalVisible}
-        onOk={handleOk}
-        okText="Done"
         footer={null}
         centered
         closable={false}
+        afterClose={getData}
       >
         <Editexpense record={editedRecord}  setIsModalVisible={setIsModalVisible}/>
       </Modal>

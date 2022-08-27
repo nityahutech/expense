8701cfd8b0b7755ff650e34b534b6a0fd2618f66
swethahutech <swetha.vijay@hutechsolutions.com>
2022-08-27 18:26:18 +0530

    fixed firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyBlxO52TTZqs4KjmDQtJbG2i0uf1GEAi-U",
+  authDomain: "fir-db-for-spring-boot-aea1a.firebaseapp.com",
+  projectId: "fir-db-for-spring-boot-aea1a",
+  storageBucket: "fir-db-for-spring-boot-aea1a.appspot.com",
+  messagingSenderId: "1030659819637",
+  appId: "1:1030659819637:web:ed6149a1784672a07cd03c",
+  measurementId: "G-F87K2WY231"
 }
 
 const app = initializeApp(firebaseConfig);

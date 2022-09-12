5a3e00d9f1924e916568bc0a5a9c94047271f5c3
Jatin <106946126+JATINHUTECH@users.noreply.github.com>
2022-09-12 17:57:21 +0530

    leavePage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExpenseFrm from "./pages/expenseFrm/AddExpense";
 import NewSider from "./components/sidebar/NewSidebar";
 import Profile from "./pages/profile/Profile";
 import Setting from "./pages/setting/Setting";
+import Leave from "./components/Leave";
 import ExpenseListpage from "./pages/ExpenseList/ExpenseListpage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tabledash from "./components/Tabledash";
@@ -26,6 +27,7 @@ function App() {
           <Route path="/New" element={<NewSider />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Setting" element={<Setting />} />
+          <Route path="/Leave" element={<Leave />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>

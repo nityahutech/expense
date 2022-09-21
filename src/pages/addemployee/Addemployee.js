5547b9b950588af234cefb24d75f196545998b1b
saswatpatel <112091094+saswatpatel@users.noreply.github.com>
2022-09-21 17:15:38 +0530

    clock with function

--- src/pages/addemployee/Addemployee.js
+++ src/pages/addemployee/Addemployee.js
@@ -3,7 +3,7 @@ import AttendanceLog from "../../components/AttendanceLog";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/NewSidebar";
 import { useAuth } from "../../contexts/AuthContext";
-import AddEmployee from "../../components/addEmployee";
+import AddEmployee from "../../components/AddEmployee";
 
 
 

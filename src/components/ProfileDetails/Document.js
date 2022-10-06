598f9f2af9d0b4bb627deedbdf31a393372afcc2
saswatpatel <112091094+saswatpatel@users.noreply.github.com>
2022-10-06 15:38:46 +0530

    document ui

--- src/components/ProfileDetails/Document.js
+++ src/components/ProfileDetails/Document.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 import { 
-  Card,
   Tabs,
-  Table,
-  Col, 
-  Row,
-  Input,
-  DatePicker,
   Button,
-  Form,
+  Modal,
+  Card,
+  Table,
+  Typography ,
   Select,
+  Input,
+  Checkbox,
+  message, 
+  Upload, 
 } from 'antd';
+
+import { 
+  UploadOutlined,
+} from '@ant-design/icons';
 import { PlusCircleOutlined } from '@ant-design/icons';
 import "../../style/Documents.css" 
 
@@ -19,13 +25,111 @@ import "../../style/Documents.css"
 
 // -------------------------------------------------------------------
 
+const idcolumns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+]
 
+
+const iddata = [
+  {
+    key: '1',
+    name: 'John Brown',
+  },
+  ]
 
 // ------------------------------------------------------------------
 
 function Document() {
 
-// -------------------------------------------------------------------
+// -------------------------------------------------------------------space for const decleration
+
+const { Title } = Typography;
+// --------------------------code for model in id tab
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+// -------------------------code for model in certificatoin tab
+  const [certMOdel, setCertModal] = useState(false);
+
+  const showCert = () => {
+    setCertModal(true);
+  };
+
+  const saveButton = () => {
+    setCertModal(false);
+  };
+
+  const cancelButton = () => {
+    setCertModal(false);
+  };
+// -----------------------code for model in work tab
+  const [workModel, setWorkModel] = useState(false);
+
+  const showWork = () => {
+    setWorkModel(true);
+  };
+
+  const saveWork = () => {
+    setWorkModel(false);
+  };
+
+  const cancelWork = () => {
+    setWorkModel(false);
+  };
+// -------------------------code for select on id
+  const { Option } = Select;
+
+  const idoption = (value) => {
+    console.log(`selected ${value}`);
+  };
+// -------------------------code for checkbox
+  const checkId = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
+// -------------------------code for uplode button
+  const props = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+  
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  
+    progress: {
+      strokeColor: {
+        '0%': '#108ee9',
+        '100%': '#87d068',
+      },
+      strokeWidth: 3,
+      format: (percent) => percent && `${parseFloat(percent.toFixed(2))}%`,
+    },
+  };
+// ---------------------------------------------------------space for the return
 
   return (
     <div
@@ -50,72 +154,167 @@ function Document() {
             }}>
 
           <Tabs defaultActiveKey="1" className='tabs'>
-
+              {/* ------------------------------------IDs tabs------------------------------------- */}
             <Tabs.TabPane tab="IDs" key="1">
-              <table style={{borderStyle:'solid',width:'100%'}}>
+            <Table 
+              columns={idcolumns} 
+              dataSource={iddata}
+              pagination={false}
+              style={{
+                margin:'0px',
+                padding:'0px',
+              }}
+              bordered  
+            />
+            <Modal 
+               title="Add Documents" 
+               open={isModalOpen}  
+               visible={isModalOpen} 
+               onOk={handleOk} 
+               onCancel={handleCancel}
+               className='idselect'
+               >
+                <Title level={5}>Select ID Type</Title>
+                <Select
+                   onChange={idoption}
+                  
+                   style={{
+                    width: '100%',
+                  }}
+                  defaultValue="Id Type"
+                >
+                  <Option>PAN Card</Option>
+                  <Option>Aadhar Card</Option>
+                  <Option>Passport</Option>
+                  <Option>Driving Licence</Option>
+                  <option>Voter ID</option>
+                  <Option>Electricity Bill</Option>
+                  <Option>Phone Bill</Option>
+                  <Option>Bank Passbook</Option>
+                  <Option>Rental Agreement</Option>
+                </Select>
 
-                <tr>
-                  <td colspan="2" style={{borderStyle:'solid',width:'25%', fontWeight:'200'}}><h1 style={{ fontWeight:'bold'}}>Proof</h1></td>
-                </tr>
 
-                <tr>
-                  <td style={{borderStyle:'solid',width:'25%'}}>Photo Id</td>
-                  <td style={{borderStyle:'solid',width:'100%'}}></td>
-                </tr>
+                <Input placeholder='Enter ID' style={{ marginTop:'25px', marginBottom:'25px' }}/>
 
-                <tr>
-                  <td style={{borderStyle:'solid',width:'25%'}}>Date Of Birth</td>
-                  <td style={{borderStyle:'solid',width:'100%'}}></td>
-                </tr>
 
-                <tr>
-                  <td style={{borderStyle:'solid',width:'25%'}}>Current Address</td>
-                  <td style={{borderStyle:'solid',width:'100%'}}></td>
-                </tr>
 
-                <tr>
-                  <td style={{borderStyle:'solid',width:'25%'}}>Premanent Address</td>
-                  <td style={{borderStyle:'solid',width:'100%'}}></td>
-                </tr>
+                <Title level={5}>Use it as proof for</Title>
+                <Checkbox onChange={checkId}>Photo ID</Checkbox>
+                <br />
+                <Checkbox onChange={checkId}>Date of Birth</Checkbox>
+                <br />
+                <Checkbox onChange={checkId}>Current Address</Checkbox>
+                <br />
+                <Checkbox onChange={checkId}>Permanent</Checkbox>
+                <br />
 
-              </table> 
-              
-              
-              
+
+                <Upload {...props}>
+                  <Button  
+                    style={{marginTop:'25px'}} 
+                    icon={<UploadOutlined />}
+                    type='primary'
+                  >
+                      Click to Upload
+                    </Button>
+                </Upload>
+              </Modal>
+              {/* --------------------Document uploded table-------- */}
+              <Button 
+                    type='text'
+                    onClick={showModal}
+                    className='idbutton'
+                  >
+                    <PlusCircleOutlined /> Add
+                  </Button>
             </Tabs.TabPane>
 
-            <Tabs.TabPane tab="Certification" key="2" className='certficationtable'>
-              <table style={{border: '1px solid #DDD',width: '100%'}}>
-                <tr>
-                  <th style={{
-                    // borderBottom: '1px solid #DDD'
-                    }}>COURSE TITLE</th>
-                  <th style={{
-                    // borderBottom: '1px solid #DDD',
-                    // borderLeft: '1px solid #DDD'
-                    }}>COURSE TYPE</th>
-                </tr>
-                <tr>
-                  <td style={{
-                    width:'50%',
-                    padding:'8px'
-                    }}>hi</td>
-                  <td style={{
-                    width:'50%',
-                    // borderLeft: '1px solid #DDD',
-                    padding:'8px'
-                    }}>hi</td>
-                </tr>
-                <tr colspan="2">
-                  <Button type='text'><PlusCircleOutlined /> Add</Button>
-                </tr>
-              </table>
+            
+
+            {/* ----------------------------------------------------------Certification tab---------------- */}
+
+            <Tabs.TabPane tab="Certification" key="2" >
+            <Modal 
+               title="Add Documents" 
+               open={certMOdel}  
+               visible={certMOdel} 
+               onOk={saveButton} 
+               onCancel={cancelButton}
+               className='certficationtable'
+              >
+                <Title level={5}>Select Course Type</Title>
+                <Select
+                   onChange={idoption}
+                  
+                   style={{
+                    width: '100%',
+                  }}
+                  defaultValue="Course Type"
+                >
+                  <Option>Graduation</Option>
+                  <Option>Post Graduation</Option>
+                  <Option>Doctorate</Option>
+                  <Option>Diploma</Option>
+                  <Option>Pre University</Option>
+                  <Option>Certification</Option>
+                  <Option>Others</Option>
+                </Select>
+
+                <Input placeholder='Enter Certification title' style={{ marginTop:'25px', marginBottom:'25px' }}/> 
+
+                <Upload {...props}>
+                  <Button  
+                    style={{marginTop:'5px'}} 
+                    icon={<UploadOutlined />}
+                    type='primary'
+                  >
+                      Click to Upload
+                    </Button>
+                </Upload>  
+
+              </Modal>
+              <Button 
+                    type='text'
+                    onClick={showCert}
+                  >
+                    <PlusCircleOutlined /> Add
+                  </Button>
             </Tabs.TabPane>
 
-            <Tabs.TabPane tab="Work" key="3">
-              Content of Tab Pane 3
-            </Tabs.TabPane>
+            {/* -------------------------------------------------Work tabs-------------------------- */}
 
+            <Tabs.TabPane tab="Work" key="3" >
+            <Modal 
+               title="Add Documents" 
+               open={workModel}  
+               visible={workModel} 
+               onOk={saveWork} 
+               onCancel={cancelWork}
+               className='worktab'
+            >
+              <Input placeholder='Enter Document Title' style={{ marginTop:'25px', marginBottom:'25px' }}/> 
+
+              <Input placeholder='Enter Document Description' style={{ marginTop:'25px', marginBottom:'25px' }}/>
+
+              <Upload {...props}>
+                  <Button  
+                    style={{marginTop:'5px'}} 
+                    icon={<UploadOutlined />}
+                    type='primary'
+                  >
+                      Click to Upload
+                    </Button>
+                </Upload>    
+
+            </Modal>
+                <Button 
+                      type='text'
+                      onClick={showWork}
+                    >
+                      <PlusCircleOutlined /> Add
+                    </Button>
+            </Tabs.TabPane>
           </Tabs>
         </div>
     </div>

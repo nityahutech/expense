2f0f72ed6ac697936e9d5a7e5c245ba27016e7b0
saswatpatel <112091094+saswatpatel@users.noreply.github.com>
2022-12-27 10:18:08 +0530

    new routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import LoginPage from "./components/LoginPage";
-import ExpenseFrm from "./pages/expenseFrm/AddExpense";
+import ExpenseFrm from "./components/ExpenceForm";
 import Setting from "./pages/setting/Setting";
 import Leave from "./pages/leave/Leave";
-import EmployeeListPage from "./pages/Employeelist/EmployeeListPage";
-import Addemployee from "./pages/addemployee/Addemployee";
+import EmployeeList from "./components/EmployeeList";
+import Addemployee from "./components/addemployee";
+import ExpenseList from "./components/Expenselist";
 import ExpenseListpage from "./pages/ExpenseList/ExpenseListpage";
 import Attendance from "./pages/Attendance/Attendance";
-import AppraisalHr from "./pages/AppraisalPageHr";
-import Payslip2 from "./pages/Payslip2";
+import AppraisalHr from "./components/quarterApp/appraisalHomePage";
+import Payslip2 from "./components/ProfileDetails/HrPaySlip";
 
 import Profile from "./components/Profilepage";
-import OnboardingPage from "./pages/OnboardingPage";
-import CompanyProfilepage from "./pages/CompanyProfilepage";
-import HalfYearGoalPage from "./pages/halfYearGoalPage";
+import Onboarding from "./components/Onboarding";
+import CompanyProfile from "./components/CompanyProfile";
+import HalfYearGoalHome from "./components/halfYearGoal/halfYearGoalHome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./App.css";
 import MainDashBoard from "./components/MainDashBoard";
 import Navbar from "./components/navbar/Navbar";
 import NewSidebar from "./components/sidebar/NewSidebar";
+import Settingpage from "./components/Settingpage";
 
 function App() {
   const page = (main, activeSubMenu, activeMenu) => {
@@ -48,19 +50,19 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/DashBoard" element={page(<MainDashBoard />, ["sub5"], ["30"])} />
-          <Route path="/Organization/Onboarding" element={<OnboardingPage />} />
-          <Route path="/CompanyProfile" element={<CompanyProfilepage />} />
+          <Route path="/Organization/Onboarding" element={page(<Onboarding />)} />
+          <Route path="/CompanyProfile" element={page(<CompanyProfile />,["32"],["32"])} />
           <Route path="/Attendance" element={<Attendance />} />
           <Route path="/Leave" element={<Leave />} />
-          <Route path="/Employee/AddEmployee" element={<Addemployee />} />
-          <Route path="/Employee/EmployeeList" element={<EmployeeListPage />} />
-          <Route path="/Employee/Payroll" element={<Payslip2 />} />
-          <Route path="/Expense/AddExpense" element={<ExpenseFrm />} />
-          <Route path="/Expense/ExpenseList" element={<ExpenseListpage />} />
+          <Route path="/Employee/AddEmployee" element={page(<Addemployee />,["sub2"],["8"])} />
+          <Route path="/Employee/EmployeeList" element={page(<EmployeeList />,["sub2"],["9"])} />
+          <Route path="/Employee/Payroll" element={page(<Payslip2 />,["sub2"],["25"])} />
+          <Route path="/Expense/AddExpense" element={page(<ExpenseFrm />,["sub1"],["2"])} />
+          <Route path="/Expense/ExpenseList" element={page(<ExpenseList />,["sub1"],["3"])} />
           <Route path="/Profile" element={page(<Profile />, ["21"], ["21"])} />
-          <Route path="/Settings" element={<Setting />} />
-          <Route path="/Appraisal/AppraisalPageHr" element={<AppraisalHr />} />
-          <Route path="/Appraisal/HalfYearGoalPage" element={<HalfYearGoalPage />} />
+          <Route path="/Settings" element={page(<Settingpage />)  } />
+          <Route path="/Appraisal/AppraisalPageHr" element={page(<AppraisalHr />,["sub4"],["20"])} />
+          <Route path="/Appraisal/HalfYearGoalPage" element={page(<HalfYearGoalHome />,["sub4"],["20a"])} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>

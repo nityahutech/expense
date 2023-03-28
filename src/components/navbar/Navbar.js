58ee4a8819de19326aac86366b671ee63995e96b
Swetha Vijay <swetha.vijay@hutechsolutions.com>
2023-03-28 13:37:46 +0530

    last refresh

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = (props) => {
       setIsRunning(false);
       return false;
     } else {
-      localStorage.setItem("lastRefresh", moment())
+      localStorage.setItem("lastRefresh", moment().format("x"))
       setButtonText()
       setIsRunning(true);
       let offset = moment().subtract(res.clockIn);
@@ -151,7 +151,7 @@ const Navbar = (props) => {
 
   const refreshTimer = () => {
     let last = localStorage.getItem("lastRefresh");
-    const diff = moment.duration(moment().diff(moment(last)));
+    const diff = moment.duration(moment().diff(moment(last, "x")));
     if(diff._milliseconds > 1200000) {
       setRefresh(!refresh)
     }
